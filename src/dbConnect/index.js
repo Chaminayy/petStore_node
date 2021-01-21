b72e35647197621006839080227360ef67111448
chaminayy <99679479@qq.com>
2021-01-21 17:33:07 +0800

    '更新'

--- src/dbConnect/index.js
+++ src/dbConnect/index.js
@@ -4,10 +4,12 @@ globalThis.userInfoTable = 'user_info'
 handleError = function () {
   let db
   db = mysql.createConnection({
-    host: '121.196.167.112',
+    // host: '121.196.167.112',
+    // database: 'pet_store',
+    host: 'localhost',
+    database: 'test',
     user: 'root',
     password: 'root',
-    database: 'test'
   })
   //连接错误，2秒重试
   db.connect(function (err) {
